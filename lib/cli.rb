26594a671493e65e0cb430bc36cbc1fd996e3fdc
Mely07 <damely.tineo07@gmail.com>
2021-05-23 15:41:05 -0400

    Display number of incorrect guesses, handle user input - if letter not found increase count, display corresp image, and call input method again.

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,4 +1,5 @@
 class UFOGame::CLI
+  @@incorrect_guesses = 0
   @@word #actual word
   @@codeword
 
@@ -9,7 +10,11 @@ class UFOGame::CLI
     puts
     @@word = generate_word
     @@codeword = @@word.gsub(/\S/, '_')
+    puts "Incorrect Guesses:"
+    puts @@incorrect_guesses
     puts @@codeword
+    puts
+    get_input 
   end
 
   def ufo
@@ -124,7 +129,27 @@ x = ["                 .
   def get_input 
     letter = gets.chomp
     letter = letter.upcase
+    search_for_letter(letter)
   end
+
+  def search_for_letter(letter) #handle input
+      if @@incorrect_guesses < 6
+        if !@@word.include?(letter)
+          @@incorrect_guesses += 1
+          puts @@codeword
+          puts "letter not found, please try again"
+          puts "Incorrect Guesses:"
+          puts @@incorrect_guesses
+          puts ufo[@@incorrect_guesses]
+          get_input
+        else
+          puts "letter found"
+          #handle letter found 
+        end
+      else 
+        puts "exceeded # of tries"
+      end 
+    end
  
 end
 
